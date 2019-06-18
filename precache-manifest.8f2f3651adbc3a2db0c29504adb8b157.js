cba43994d59c890320eaa6ae5851f51d3ea624ea
ahmad13kurd <evil13kurd@hotmail.com>
2019-06-18 14:18:37 +0200

    Updates

---
+++ precache-manifest.8f2f3651adbc3a2db0c29504adb8b157.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f11b653809345fde90a9220e9f1f9204",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "eff132b8e80b4c87023f",
+    "url": "/weatherapp/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f6204d0219031370bd9d",
+    "url": "/weatherapp/static/css/main.148d96e8.chunk.css"
+  },
+  {
+    "revision": "eff132b8e80b4c87023f",
+    "url": "/weatherapp/static/js/2.e597a5a4.chunk.js"
+  },
+  {
+    "revision": "f6204d0219031370bd9d",
+    "url": "/weatherapp/static/js/main.b19255ea.chunk.js"
+  },
+  {
+    "revision": "5bfbd7a83723d1d6555c",
+    "url": "/weatherapp/static/js/runtime~main.e7dc3139.js"
+  }
+]);
